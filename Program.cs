5802589daac53858d94447cba2f04e554849ceaf
Mateusz Małolepszy <mateusz.malolepszy@gmail.com>
2018-05-28 23:38:13 +0200

    new logger modules

--- Program.cs
+++ Program.cs
@@ -4,24 +4,77 @@ using System.Reflection;
 using System.Security.Principal;
 using log4net;
 using log4net.Config;
+using NLog;
+using Serilog;
 
 namespace log_in_module
 {
     
     class Program
     {
-         private static readonly log4net.ILog log = log4net.LogManager.GetLogger
-        (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
        static void Main(string[] args)
         {
-            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log in module.config"));
+            // Log4net lg4net = new Log4net();
+            // lg4net.Login();
+
+            Nlog nlg = new Nlog();
+            nlg.Login();
+
+            // Serilog srlg = new Serilog();
+            // srlg.Login();
+        }          
+    }
+    class Log4net
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+        (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+         public void Login()
+         {
+            var logRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
             ILog log = log4net.LogManager.GetLogger(typeof(Program));
             log.Debug("This is a debug message");
             log.Warn("This is a warn message");
             log.Error("This is a error message");
             log.Fatal("This is a fatal message");
-            Console.ReadLine();
+         }
+    }
+    class Nlog
+    {
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+        public void Login()
+        {
+            // log.Info("This is an info message");
+            // log.Debug("This is a debug message");
+            // log.Warn("This is a warn message");
+            // log.Error("This is an error message");
+            // log.Fatal("This is a fatal message");
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var logfile = new NLog.Targets.FileTarget() { FileName = "file.txt", Name = "logfile" };
+            var logconsole = new NLog.Targets.ConsoleTarget() { Name = "logconsole" };
+
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Info, logconsole));
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Debug, logfile));
+
+            NLog.LogManager.Configuration = config;
+        }
+    }
+    class Serilog
+    {
+        public void Login()
+         {
+            // Create Logger
+            var log = new LoggerConfiguration()
+                    .WriteTo.Console()
+                    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+            // Output logs
+            log.Warning("This is a warning message");
+            log.Information("This is an info message");
+            log.Error("This is an error message");
+            log.Fatal("This is a fatal message");
         }
     }
 }
